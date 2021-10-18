cc0717149fdb9c36ecef7bafa625aef3697dbf41
ashn <me@ashn.dev>
2021-10-18 18:58:05 -0400

    Cleanup code for source release

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import argparse
 import time
 
-from PIL import Image  # type: ignore
+from PIL import Image
 
 # Unicode Braille Patterns
 # ========================
@@ -65,9 +65,8 @@ def image_to_braille(path, width=160, height=120):
     img = Image.open(path)
     img = img.convert("1")  # 1-bit color (a.k.a black & white)
     img = img.resize((width, height))
-    #img.save("out.png")
-
-    s = chr(27) + "[2J"
+    ESC = chr(27)  # ANSI ESC
+    s = f"{ESC}[H{ESC}[2J"  #  HOME; CLEAR SCREEN
     for y in range(0, img.height, 4):
         for x in range(0, img.width, 2):
             bits = 0b00000000
@@ -89,8 +88,7 @@ def image_to_braille(path, width=160, height=120):
                 bits |= 0b10000000
             s += chr(0x2800 | bits)
         s += "\n"
-    print(s, end="\n")
-
+    return s
 
 
 def main():
@@ -98,24 +96,42 @@ def main():
         description="Transform PNGs in DIRECTORY from RGB into black & white."
     )
     parser.add_argument("directory", metavar="DIRECTORY", type=str)
+    parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
-    #image_to_braille(sorted(Path(args.directory).glob("*.png"))[5770])
 
-    FPS=30
-    accum = 0
-    time.sleep(0.15) # for demo
-    def next_frame():
-        nonlocal accum
+    # Bad Apple is 30 FPS. To make this script work with any arbitrary video
+    # the FPS of the video would need to be explicitly passed as a program
+    # argument. For the purposes of this mini-project this is a WONTFIX.
+    FPS = 30
+    # Time accumulator. For each frame rendered, 1 / FPS is subtracted from the
+    # accumulator and the time to execute the frame-rendering logic is added to
+    # the accumulator. Draw calls are only permitting once every 1 / FPS
+    # seconds, and any excess time is captured in the new value of the
+    # accumulator. This systems leads to a semi-stable frame rate without
+    # having to worry about de-sync.
+    accumulator = 0
+
+    def next_frame(frame, accumulator):
         start = time.time()
-        while (time.time() - start) + accum < 1/FPS:
+        braille = image_to_braille(frame)
+        # Wait until 1 / FPS seconds have elapsed since the last frame render.
+        while (time.time() - start) + accumulator < 1 / FPS:
             time.sleep(0.001)
-        #print(frame, f"ACCUM: {accum}")
-        image_to_braille(frame)
-        accum += time.time() - start - 1/FPS
+        # Perform draw calls.
+        print(braille, end="")
+        if args.debug:
+            print(f"Current Frame: {frame}")
+            print(f"Accumulator: {accumulator:.5f}")
+        # Return the new value of the accumulator.
+        return accumulator + time.time() - start - 1 / FPS
 
-    frames = sorted(Path(args.directory).glob("*.png"))
-    for frame in frames:
-        next_frame()
+    # Pause for a brief period of time so that I can start the script and then
+    # hit play on the bad-apple video without the script pulling many frames
+    # ahead of the video. XXX: This is not a very good way to sync the video
+    # and terminal streams.
+    time.sleep(0.15)
+    for frame in sorted(Path(args.directory).glob("*.png")):
+        accumulator = next_frame(frame, accumulator)
 
 
 if __name__ == "__main__":
